d2ec81d4f1f4e097940772797bcb283b6dd350fb
harsh6921 <pachorihimanshu0@gmail.com>
2025-10-25 18:00:36 +0530

    day 1

---
+++ src/customer/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+import MainCrosel from '../../components/HomeCrosel/MainCrosel';
+
+const HomePage = () => {
+    return (
+        <div><MainCrosel/> 
+        <div>
+        other section 
+        </div>
+        </div>
+    )
+}
+export default HomePage;
---
+++ src/customer/components/HomeCrosel/MainCrosel.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import  { MainCroselData } from './MainCroselData';
+
+
+
+const MainCrosel = () => {
+   
+    const items = MainCroselData.map((item) => <img className='curser-pointer h-96 w-full'
+     role='presentation' src={item.image} alt="carousel-item" />)
+
+   return (
+    <AliceCarousel
+        items={items}
+        disableButtonsControls
+        autoPlay
+        infinite
+        autoPlayInterval={1000} 
+    
+/>)
+};
+
+
+export default MainCrosel;
